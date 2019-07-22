73b0488c36477591dcd65f8b557a2150340231e1
trungdo <trungdovan87@gmail.com>
2019-07-22 20:05:14 +0700

    add getHeader() and getPrincipalId

--- event-utils.js
+++ event-utils.js
@@ -8,13 +8,19 @@ const getBody = (event) => {
   return JSON.parse(event.body)
 }
 
+const getHeader = (event, header) => event.headers[header]
+
 const getMutiValueQueryParam = (event, param) => event.multiValueQueryStringParameters[param]
 
 const getValueQueryParam = (event, param) => event.queryStringParameters[param]
+
+const getPrincipalId = (event) => event.requestContext.authorizer.principalId
 
 module.exports = {
   getPathParam,
   getBody,
   getMutiValueQueryParam,
   getValueQueryParam,
+  getHeader,
+  getPrincipalId,
 }
