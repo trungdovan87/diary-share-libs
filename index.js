61b207522cca5af5c89247328db1d20cffbeb7c0
trungdo <trungdovan87@gmail.com>
2019-07-24 16:26:30 +0700

    remove jwt_utils in lib

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const gatewayUtils = require('./gateway-utils')
-const jwtUtils = require('./jwt-utils')
 const eventUtils = require('./event-utils')
 
 module.exports = {
-  gatewayUtils,
-  jwtUtils,
+  gatewayUtils,  
   eventUtils
 }
